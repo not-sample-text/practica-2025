79544e1780019daf7d3b621f1189bb17b14ecb62
Viorel Filip <viorelfilip@outlook.com>
2025-07-09 11:21:23 +0300

    Implement WebSocket server and client messaging functionality

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 const Koa = require('koa');
 const fs = require('fs');
-const app = new Koa();
 const jwt = require('jsonwebtoken');
+const route = require('koa-route');
+const websockify = require('koa-websocket');
+
+const clients = new Map(); // Store connected clients
+const option = {
+  httpOnly: false,         // ✅ dacă vrei să-l vezi din JS (document.cookie)
+  sameSite: 'lax',         // ✅ Lax sau Strict sau None (vezi explicații jos)
+  secure: false,           // ✅ false pe localhost (true doar pe HTTPS)
+  domain: 'localhost',     // opțional; se poate omite pe localhost
+  path: '/',               // disponibil pe tot site-ul
+  maxAge: 1000 * 60 * 60,  // opțional: 1 oră
+};
 const secretKey = '564798ty9GJHB%^&*(KJNLK';
+const app = websockify(new Koa());
+
+app.ws.use(route.all('/ws', function (ctx) {
+    // `ctx` is the regular koa context created from the `ws` onConnection `socket.upgradeReq` object.
+    // the websocket is added to the context on `ctx.websocket`.
+    const { username } = jwt.verify(ctx.cookies.get('token'), secretKey);
+    clients.set(ctx.cookies.get('token'), ctx.websocket);
+
+    ctx.websocket.send(`token–Hello ${username}, welcome to the WebSocket server!`);
+    ctx.websocket.on('message', function (message) {
+        // do something with the message from client
+        console.log(`Received ws from client: ${message}`);
+        // broadcast the message to all connected clients
+        clients.forEach((client, token) => {
+            if (client.readyState === client.OPEN) {
+                client.send(`${token}–${message}`);
+            }
+        });
+    });
+}));
 
 app
     .use(async (ctx, next) => {
@@ -22,7 +53,7 @@ app
             const content = fs.readFileSync('./welcome.html', 'utf-8');
             const response = content.replace('@username', username);
             const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
-            ctx.cookies.set('token', token);
+            ctx.cookies.set('token', token, option);
             return ctx.body = response;
         }
         await next(); // Call the next middleware
